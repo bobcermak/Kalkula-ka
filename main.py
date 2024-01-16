7d4f960814dd3a40a3eee4b8beb74565cc6393b2
bobcermak <bohuslavcermak62@gmail.com>
2024-01-16 17:36:48 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import time
+
+
+while True:
+    
+    try:
+        number1 = float(input("Zadejte číslo: "))
+        number2 = float(input("Zadejte číslo: "))
+        symbol = input("Zadejte symbol: ")
+        
+        def math():
+            if symbol == "+":
+                result = float(number1) + float(number2)
+                print("Váš výsledek je " + str(result))
+            
+            elif symbol == "-":
+                result = float(number1) - float(number2)
+                print("Váš výsledek je " +  str(result))
+                
+            elif symbol == "*":
+                result = float(number1) * float(number2)
+                print("Váš výsledek je " +  str(result))
+                 
+            elif symbol == "/":
+                result = float(number1) / float(number2)
+                print("Váš výsledek je " + str(result))
+                
+                
+        
+           
+                    
+                
+                 
+        math()
+        choice = str(input("Budete mě ještě znova potřebovat?: (y/n): "))
+        if choice == "n":
+            break
+        else:
+            print("Jdme na další čísla :)")
+        
+        
+        
+        
+    except ValueError:
+        time.sleep(1)
+        print("Zadal jste špatný znak!")
